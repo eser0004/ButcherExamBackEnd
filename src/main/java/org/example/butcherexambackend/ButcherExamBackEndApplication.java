5026e9811bde52e589c651e5720f872656a95b8e
DekaSkole <eser0004@stud.kea.dk>
2024-12-04 12:38:12 +0100

    first commit

---
+++ src/main/java/org/example/butcherexambackend/ButcherExamBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.butcherexambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherExamBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButcherExamBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/butcherexambackend/ButcherExamBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.butcherexambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcherExamBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
